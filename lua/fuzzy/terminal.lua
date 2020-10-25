5de1c0cc45ec2c40afd63b692cf6d7f2a9b6f714
Loric André <loric@localhost.localdomain>
2020-10-25 20:44:11 +0100

    Use fzy and tweak it so it uses the right number of lines

--- lua/fuzzy/terminal.lua
+++ lua/fuzzy/terminal.lua
@@ -7,6 +7,7 @@ local TerminalFuzzy = {}
 CURRENT_TERMINAL_FUZZY = nil
 
 local FUZZY_FZF_CMD = 'fzf'
+local FUZZY_FZY_CMD = 'fzy -p '
 
 function __FUZZY_TERMINAL_CLOSER()
   CURRENT_TERMINAL_FUZZY.closer()
@@ -18,7 +19,6 @@ function TerminalFuzzy.new(stdin, fuzzy_finder, handler)
   if type(stdin) == 'table' then
     stdin = string.format('printf "%s"', table.concat(stdin, '\n'))
   end
-  local cmd = string.format('%s | %s', stdin, fuzzy_finder)
   -- Check for options
   -- should be set as vim.g.fuzzy_options = {location = "center", width = 50, height = 50}
   local options = vim.g.fuzzy_options or {}
@@ -41,7 +41,8 @@ function TerminalFuzzy.new(stdin, fuzzy_finder, handler)
     local height = options.height
     win_height = math.ceil(vim.api.nvim_get_option('lines')*height/100)
   end
-  local buf, win, closer = floating.floating_terminal(cmd, handler, win_width, win_height, loc) 
+  local cmd = string.format('%s | %s', stdin, fuzzy_finder)
+  local buf, win, closer = floating.floating_terminal(cmd, handler, win_width, win_height, loc)
 
   vim.api.nvim_buf_set_keymap(buf, 'i', '<esc>',  '<cmd> lua __FUZZY_TERMINAL_CLOSER()<CR>', {})
   vim.api.nvim_buf_set_keymap(buf, 'i', '<C-c>',  '<cmd> lua __FUZZY_TERMINAL_CLOSER()<CR>', {})
@@ -60,7 +61,12 @@ function TerminalFuzzy.fzf(stdin, handler)
 end
 
 function TerminalFuzzy.fzy(stdin, handler)
-  TerminalFuzzy.new(stdin, 'fzy', function(lines)
+  if vim.g.fuzzy_options.height then
+    local height = vim.g.fuzzy_options.height
+    win_height = math.ceil(vim.api.nvim_get_option('lines')*height/100)
+    FUZZY_FZY_CMD = FUZZY_FZY_CMD .. ' -l ' .. tostring(win_height - 1) -- fzy takes the last -l field into account
+  end
+  TerminalFuzzy.new(stdin, FUZZY_FZY_CMD, function(lines)
     require'fuzzy.helpers'.tprint(lines)
     handler(lines[1])
   end)
