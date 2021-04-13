68856cca4ddd50fc9e471228b9c149e08f1da389
amirrezaask <raskarpour@gmail.com>
2021-04-14 00:12:03 +0430

    some more refactor and remove old unused/unfinished algorithms

--- lua/fuzzy/init.lua
+++ lua/fuzzy/init.lua
@@ -1,10 +1,8 @@
 local fuzzy = require('fuzzy.lib')
 local helpers = require('fuzzy.lib.helpers')
 local bin = require('fuzzy.lib.source.binary')
-local drawer = require('fuzzy.lib.drawer')
 local file_finder = require'fuzzy.lib.source.file_finder'
 local grep = require'fuzzy.lib.source.grep'
-local projects = require'fuzzy.lib.source.projects'
 
 M = {}
 
@@ -156,29 +154,6 @@ function M.rg(opts)
 end
 
 
-function M.buffers(opts)
-  local _buffers = {}
-  local function buffer_state(buf)
-    if vim.api.nvim_buf_is_loaded(buf) then
-      return 'L'
-    else
-      return 'U'
-    end
-  end
-  for _,b in ipairs(vim.api.nvim_list_bufs()) do
-    if 1 == vim.fn.buflisted(b) then
-      table.insert(_buffers, string.format("[%s] %s:%s", buffer_state(b), b, vim.api.nvim_buf_get_name(b)))
-    end
-  end
-  fuzzy.new {
-    handler = function(line)
-      local buffer_name = vim.split(line, ':')[2]
-      vim.cmd(string.format('buffer %s', buffer_name))
-    end,
-    source = _buffers,
-  }
-end
-
 function M.buffer_lines(opts)
   local filename = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
   local source = vim.split(grep.read_file(filename), '\n')
@@ -209,83 +184,6 @@ function M.cd(opts)
     handler = function(line)
       vim.cmd(string.format('cd %s', line))
     end,
-  }
-end
-
-function M.colors(opts)
-  fuzzy.new {
-    handler = function(color)
-      vim.cmd(string.format('colorscheme %s', color))
-    end,
-    source = vim.fn.getcompletion('', 'color'),
-  }
-end
-
-function M.commands(opts)
-  fuzzy.new {
-    source = vim.fn.getcompletion('', 'command'),
-    handler = function(command)
-      vim.cmd(command)
-    end,
-  }
-end
-
-function M.mru(opts)
-  fuzzy.new {
-    source = vim.split(vim.fn.execute('oldfiles'), '\n'),
-    handler = function(file)
-      vim.cmd (string.format('e %s', vim.split(file, ':')[2]))
-    end,
-  }
-end
-
-function M.history(opts)
-  opts = opts or {}
-  fuzzy.new {
-    source = vim.split(vim.fn.execute('history cmd'), '\n'),
-    handler = function(command)
-      print(vim.split(command, ' ')[2])
-      vim.cmd(vim.split(command, ' ')[2])
-    end,
-  }
-end
-
-function M.projects(opts)
-  opts = opts or {}
-  local project_list = projects.list_projects(opts.locations)
-  fuzzy.new {
-    source = function()
-      return project_list 
-    end,
-    handler = function (path)
-      vim.cmd(string.format([[ cd %s ]], path))
-    end
-  }
-end
-function M.help(opts)
-  opts = opts or {}
-  fuzzy.new {
-    source = function()
-      return vim.fn.getcompletion('', 'help')
-    end,
-    sorter = function(query, collection)
-      return vim.fn.getcompletion(query, 'help')
-    end,
-    handler = function(line)
-      vim.cmd ([[ h ]] .. line)
-    end
-  }
-end
-
-function M.mappings(opts)
-  opts = opts or {}
-  local mappings = vim.split(vim.fn.execute('map'), '\n')
-  fuzzy.new {
-    source = mappings,
-    handler = function(line)
-      local command = vim.split(line, ' ')[3]
-      vim.fn.execute(command)
-    end
   }
 end
 
--- lua/fuzzy/lib/alg/levenshtein.lua
+++ lua/fuzzy/lib/alg/levenshtein.lua
@@ -1,6 +1,59 @@
-local quicksort = require'fuzzy.lib.alg.quicksort'
-
 local __ngrams_cache = {}
+
+local function quicksort(arr, l, h) 
+  local function partition(t, low, high)
+    local i = (low-1)
+    local pivot = t[high]
+
+    for j=low,high-1 do
+      if t[j].score >= pivot.score then
+        i = i+1
+        t[i], t[j] = t[j], t[i]
+      end
+    end 
+    t[i+1], t[high] = t[high], t[i+1]
+    return (i+1)
+  end
+
+  local function table_with_size(size, default_value)
+    local t = {}
+    for i=1,size do
+      table.insert(t, default_value)
+    end
+    return t
+  end
+
+  local size = h - l + 1
+  local stack = table_with_size(size, 0) 
+ 
+  local top = -1
+  top = top + 1
+  stack[top] = l 
+  top = top + 1
+  stack[top] = h 
+
+  while top >= 0 do 
+    h = stack[top] 
+    top = top - 1
+    l = stack[top] 
+    top = top - 1
+    local p = partition(arr, l, h ) 
+    if p-1 > l then 
+      top = top + 1
+      stack[top] = l 
+      top = top + 1
+      stack[top] = p - 1
+    end
+    if p + 1 < h then 
+      top = top + 1
+      stack[top] = p + 1
+      top = top + 1
+      stack[top] = h 
+    end
+  end
+  return arr
+end
+
 
 local function ngrams_of(s, n)
   n = n or 3
